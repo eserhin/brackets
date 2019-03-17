3e1532890da2608d49f4941e6470dbb426994a32
Vyacheslav Turok <veserhin@gmail.com>
2019-03-17 19:16:49 +0300

    first attempt

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+	var config=[];
+    for(var q=0;q<bracketsConfig.length;q++){
+    	config.push(bracketsConfig[q][0]+bracketsConfig[q][1]);
+    }
+    while(config.some(q => str.includes(q))) {
+        for(var q = 0; q < config.length; q++) {
+                    str = str.replace(config[q],"")
+        }
+    }
+    return !str.length;
 }
